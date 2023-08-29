8bc783fbefd57fed042851c25cec373f3af1a302
Fatimah <aishafhonore@gmail.com>
2023-08-29 10:17:20 -0500

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.get("/greeting/:name", (request, response) => {
   response.send("<h1>What's up</h1>" + request.params.name);
 });
 
+app.get("/tip/:total/:tipPercentage", (request, response) => {
+  response.send(
+    "<h1>Your tip is:</h1>" +
+      request.params.total * (request.params.tipPercentage / 100)
+  );
+});
+
 app.listen(3000, () => {
   console.log("Greeting initialized");
 });
